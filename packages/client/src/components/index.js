cad1684381836ee82c178e62f821a42e74b99fc2
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-11-27 23:37:06 -0500

    Barrel export to components and pages

---
+++ packages/client/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Background } from './Background'
+export { default as Snowfall } from './Snowfall'
+export { default as Button } from './Button'
+export { default as Bauble } from './Bauble'
