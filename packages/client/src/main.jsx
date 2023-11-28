f37526107eb4fd55634caf21f24b4854e42ed0b4
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-11-27 23:34:29 -0500

    Add Toaster component in main

--- packages/client/src/main.jsx
+++ packages/client/src/main.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { Toaster } from 'sonner';
 
-import '@unocss/reset/tailwind-compat.css'
-import 'virtual:uno.css'
-import './index.css'
+import '@unocss/reset/tailwind-compat.css';
+import 'virtual:uno.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
-)
+    <Toaster richColors />
+  </>
+);
