f111948a1b43ee1d7a95d1cddcf0c17109d0d3a9
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-11-29 18:29:35 -0500

    Event to start game

---
+++ packages/server/events/admin/start-game.js
@@ -0,0 +1,35 @@
+import { getRoomsByGame } from '../../room.js'
+
+const event = ({ store, socket, io }, cb = () => {}) => {
+  // TODO: mirar si se envia por cb o game:alert
+
+  if (store.teams.size === 0) {
+    return cb({
+      status: false,
+      message: 'Primero debe crear los equipos, para iniciar el juego'
+    })
+  }
+
+  const roomsGame = getRoomsByGame(store.escapeGame)
+  if (!roomsGame || roomsGame.length === 0) {
+    return cb({
+      status: false,
+      message: 'No se puede iniciar el juego, no existe rooms'
+    })
+  }
+
+  ;[...store.teams.entries()].forEach(([key, team]) => {
+    store.teams.set(key, {
+      ...team,
+      currentRoom: 0,
+      rooms: [{ timeStart: +new Date(), attempts: 0, finish: false, points: 0 }]
+    })
+  })
+
+  const firstRooom = roomsGame[0]
+  io.to('game').emit('game:start', firstRooom)
+
+  cb({ status: true })
+}
+
+export default event
