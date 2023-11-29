32953ab5241e7e698ce55cd2ea3086a4896e4d31
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-11-28 22:52:40 -0500

    Add property nick and isLeader in Zustand store

--- packages/client/src/store/useGame.js
+++ packages/client/src/store/useGame.js
@@ -3,15 +3,22 @@ import { io } from 'socket.io-client'
 
 import { states } from '@/utils'
 
-export default create((set, get) => ({
-  socket: io(import.meta.env.VITE_URL_SERVER_SOCKET, {
-    autoConnect: false,
-    closeOnBeforeunload: false
-  }),
+const socket = io(import.meta.env.VITE_URL_SERVER_SOCKET, {
+  autoConnect: false,
+  closeOnBeforeunload: false
+})
 
-  state: states.INITIAL,
-  setState: newState => {
+export default create((set, get) => ({
+  socket: socket,
+
+  state: [states.INITIAL, {}],
+  isLeader: false,
+  nick: '',
+
+  setState: (newState, props) => {
     const exist = Object.values(states).includes(newState)
-    if (exist) return set({ state: newState })
-  }
+    if (exist) return set({ state: [newState, props] })
+  },
+  setIsLeader: isLeader => set({ isLeader }),
+  setNick: nick => set({ nick })
 }))
