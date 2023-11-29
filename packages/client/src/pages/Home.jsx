ff6f487ed53e68abed26b364896ad7c9eb6393f9
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-11-28 22:53:55 -0500

    Set nick in Zustand store when join user

--- packages/client/src/pages/Home.jsx
+++ packages/client/src/pages/Home.jsx
@@ -1,11 +1,11 @@
 import { toast } from 'sonner';
 
-import { Background, Button } from '@/components';
+import { Button, Input } from '@/components';
 import { useGame } from '@/store';
 import { states } from '@/utils';
 
 const Home = () => {
-  const { socket, setState } = useGame();
+  const { socket, setNick, setState } = useGame();
 
   function handlerJoin(e) {
     e.preventDefault();
@@ -14,33 +14,31 @@ const Home = () => {
     socket.emit('game:join-user', username, (status) => {
       if (!status) return toast.error(`Ya existe el nick "${username}"`);
 
+      setNick(username);
       setState(states.WAIT);
     });
   }
 
   return (
-    <Background>
-      <div className="text-white flex justify-center items-center font-primary flex-col gap-10 z-50">
-        <header className="flex flex-col justify-center items-center text-8xl">
-          <h2 className="text-primary">ESCAPE</h2>
-          <h2>ROOM</h2>
-        </header>
+    <div className="text-white flex justify-center items-center font-primary flex-col gap-10 z-50">
+      <header className="flex flex-col justify-center items-center text-5xl sm:text-8xl">
+        <h2 className="text-primary">ESCAPE</h2>
+        <h2>ROOM</h2>
+      </header>
 
-        <form
-          onSubmit={handlerJoin}
-          className="flex gap-2 flex-col font-secondary text-2xl"
-        >
-          <input
-            required
-            type="text"
-            placeholder="Ingrese username"
-            className="p-3 rounded-md text-dark"
-            name="username"
-          />
-          <Button>INGRESAR</Button>
-        </form>
-      </div>
-    </Background>
+      <form
+        onSubmit={handlerJoin}
+        className="flex gap-2 flex-col font-secondary sm:text-2xl"
+      >
+        <Input
+          type="text"
+          placeholder="Ingrese username"
+          name="username"
+          autoFocus
+        />
+        <Button>INGRESAR</Button>
+      </form>
+    </div>
   );
 };
 
