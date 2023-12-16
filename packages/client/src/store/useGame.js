70eaf637546bb96bfa7cf013bd74a699c74b6b7f
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-12-16 00:21:40 -0500

    Add persist in Zustand store

--- packages/client/src/store/useGame.js
+++ packages/client/src/store/useGame.js
@@ -1,4 +1,5 @@
 import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
 import { io } from 'socket.io-client'
 
 import { states } from '@/utils'
@@ -11,19 +12,28 @@ const socket = io(import.meta.env.VITE_URL_SERVER_SOCKET, {
 // TODO: borrar a futuro
 window.socketCL = socket
 
-export default create((set, get) => ({
-  socket: socket,
+export default create(
+  persist(
+    (set, get) => ({
+      socket: socket,
 
-  state: [states.INITIAL, {}],
-  isLeader: false,
-  nick: '',
-  idTeam: '',
+      state: ['', {}],
+      isLeader: false,
+      nick: '',
+      idTeam: '',
 
-  setState: (newState, props) => {
-    const exist = Object.values(states).includes(newState)
-    if (exist) return set({ state: [newState, props] })
-  },
-  setIsLeader: isLeader => set({ isLeader }),
-  setNick: nick => set({ nick }),
-  setIdTeam: idTeam => set({ idTeam })
-}))
+      setState: (newState, props) => {
+        const exist = Object.values(states).includes(newState)
+        if (exist) return set({ state: [newState, props] })
+      },
+      setIsLeader: isLeader => set({ isLeader }),
+      setNick: nick => set({ nick }),
+      setIdTeam: idTeam => set({ idTeam })
+    }),
+    {
+      name: 'CLER:user',
+      storage: createJSONStorage(() => sessionStorage),
+      partialize: state => ({ nick: state.nick, idTeam: state.idTeam })
+    }
+  )
+)
