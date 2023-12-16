7a6a58810950d886fb1e42af96f16b6b9b508ad6
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-12-16 15:23:13 -0500

    Add page to loading layout

---
+++ packages/client/src/pages/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="text-white flex h-full justify-center items-center font-secondary text-4xl flex-col gap-2 max-h-[80%] z-50 bg-dark/40 max-w-2xl w-full p-4">
+      Cargando...
+    </div>
+  );
+};
+
+export default Loading;
